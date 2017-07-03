308c6e5322ac772288863bf892b1912098b24e02
C Mayank Dogra <cmayankdogra@Mayanks-Macbook.local>
2017-07-03 18:35:04 +0530

    Added a function to read details of your own account

--- main.py
+++ main.py
@@ -5,6 +5,31 @@ ACCESS_TOKEN = '5683656431.a4b1c44.182dcfe7f7124b2d9b873838cfa886bc'
 BASE_URL = 'https://api.instagram.com/v1/'
 
 
+
+
+
+def self_info():
+    request_url = (BASE_URL + 'users/self/?access_token=%s') % (ACCESS_TOKEN)
+    print "GET request url: %s" % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info["meta"]["code"] == 200:
+        if len(user_info["data"]):
+            print "Username: %s" %(user_info["data"]["username"])
+            print "Full Name: %s" % (user_info["data"]["fullname"])
+            print "Bio: %s" % (user_info["data"]["bio"])
+            print "Website: %s" % (user_info["data"]["website"])
+            print "Number of followers: %s" % (user_info["data"]["count"]["followed_by"])
+            print "Number of post: s" % (user_info["data"]["count"]["media"])
+            print "Number of people you are following: %s" % (user_info["data"]["count"]["follows"])
+        else:
+            print "User does not exist!!!"
+    else:
+        print "INVALID REQUEST!!!"
+        print "Please check the request"
+
+
+
 def start_bot():
     while True:
         print '\n'
